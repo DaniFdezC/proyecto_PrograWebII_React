22af6b5022d9655aff56764bdc4f27cc1fd44149
Dani <dani96sk@hotmail.com>
2024-02-26 15:12:44 +0100

    Creado nuevo front

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,22 +1,16 @@
-import { useState } from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './components/Login/Login';
-import Dashboard from './components/Dashboard/Dashboard';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './components/Login/Login'
 
 function App() {
+
   return (
     <>
-        <Router>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/dashboard" component={Dashboard} />
-      </Switch>
-    </Router>
-    <Login/>
+      <Login/>
     </>
-
-  );
+  )
 }
 
-export default App;
+export default App
--- Front/src/components/LoginForm/LoginForm.jsx
+++ Front/src/components/LoginForm/LoginForm.jsx
@@ -18,3 +18,5 @@ const LoginForm = () => {
 };
 
 export default LoginForm;
+
+
